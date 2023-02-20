da4246ac80e10e58d841dc4b3cf5c88958f5a273
Meetyp <meetyogeshkumar.patel@flemingcollege.ca>
2023-02-20 10:53:24 -0500

    Update

--- Lab 4.py
+++ Lab 4.py
@@ -1,10 +1,13 @@
 from log_analysis import get_log_file_path_from_cmd_line, filter_log_by_regex
 import pandas as pd
+import re
 
 def main():
     log_file = get_log_file_path_from_cmd_line(1)
     # records = filter_log_by_regex(log_file, 'SRC=(.*?) DST=(.*?) LEN=(.*?) ', print_summary=True, print_records=True)
     dpt_tally = tally_port_traffic(log_file)
+    generate_invalid_user_report(log_file)
+    generate_source_ip_log(log_file, "220.195.35.40")
 
     for dpt, count in dpt_tally.items():
         if count > 100:
@@ -34,11 +37,38 @@ def generate_port_traffic_report(log_file, port_number):
 
 # TODO: Step 11
 def generate_invalid_user_report(log_file):
-    return
+    regex = r"^(.{6}) (.{8}) .*user (.+) .*from (.+)"
+    captured_data = filter_log_by_regex(log_file, regex)[1]
+
+    report_df = pd.DataFrame(captured_data)
+    report_header = ('Date', 'Time', 'Username', 'IP Address')
+    report_df.to_csv('invalid_users.csv', index=False, header=report_header)
 
 # TODO: Step 12
 def generate_source_ip_log(log_file, ip_address):
-    return
+    regex = r"^.* SRC=220\.195\.35\.40 .*\)"
+    captured_data = filter_log_by_regex(log_file, regex)
+
+    report_df = pd.DataFrame(captured_data)
+
+    ip = re.sub("\.", "_", ip_address)
+    report_df.to_csv(f'source_ip_{ip}.log', index=False, header=None)
+
+    with open('source_ip_220_195_35_40.log', 'r') as file:
+        contents = file.read()
+
+    modified_contents = contents.replace(',', '')
+
+    with open('source_ip_220_195_35_40.log', 'w') as file:
+        file.write(modified_contents)
+
+    with open('source_ip_220_195_35_40.log', 'r') as file:
+        contents = file.read()
+
+    modified_contents = contents.replace('"', '')
+
+    with open('source_ip_220_195_35_40.log', 'w') as file:
+        file.write(modified_contents)
 
 if __name__ == '__main__':
     main()
